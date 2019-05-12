1ddd9a538f5a38f01805d6c521b315a19643db0d
Ian Carlos <carlos.ian007@gmail.com>
2019-05-11 23:59:21 -0300

    Style(post): Add the styles in another file

---
+++ src/components/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  author: {
+    color: '#ccc',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  container: {
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    marginHorizontal: 15,
+    marginTop: 15,
+    padding: 10,
+  },
+  text: {
+    fontSize: 12,
+    marginTop: 10,
+  },
+  title: {
+    color: '#262626',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
---
+++ src/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F27D7D',
+    flex: 1,
+  },
+  header: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    height: 50,
+    justifyContent: 'center',
+  },
+  headerTitle: {
+    color: '#F27D7D',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
